5b40453d3bfe67e9cdb821e3751c65d27aaaffc4
ceoehis <ceo.ehis@outlook.com>
2018-04-27 13:17:20 +0100

    add update functionality

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -156,12 +156,54 @@ exports.author_update_get = function (req, res, next) {
       err.status = 404;
       return next(err);
     }
-    console.log(results);
     res.render('author_form', { title: 'Update Author', author: results.author })
   });
 };
 
 // Handle Author update on POST.
-exports.author_update_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Author update POST');
-};
+exports.author_update_post = [
+
+  // Validate fields
+  body('first_name').isLength({ min: 1 }).trim().withMessage('First name must be specified')
+    .isAlphanumeric().withMessage('First name must contain only alphanumeric characters'),
+
+  body('family_name').isLength({ min: 1 }).trim().withMessage('Family name must be specified')
+    .isAlphanumeric().withMessage('Family name must contain only alphanumeric characters'),
+
+  body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true }).isISO8601(),
+
+  body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true }).isISO8601(),
+
+  // Sanitize fields
+  sanitizeBody('first_name').trim().escape(),
+  sanitizeBody('family_name').trim().escape(),
+  sanitizeBody('date_of_birth').toDate(),
+  sanitizeBody('date_of_death').toDate(),
+
+  // process request after validation
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    // if there are errors
+    if (!errors.isEmpty()) {
+      res.render('author_form', { title: 'Create Author', author: req.body, errors: errors.array() });
+      return;
+    } else {
+      // update the record
+      var author = new Author({
+        first_name: req.body.first_name,
+        family_name: req.body.family_name,
+        date_of_birth: req.body.date_of_birth,
+        date_of_death: req.body.date_of_death,
+        _id: req.params.id
+      });
+      Author.findByIdAndUpdate(req.params.id, author, {}, function(err, theauthor) {
+        if(err) { return next(err); }
+
+        res.redirect(theauthor.url);
+      })
+    }
+
+  }
+
+];
