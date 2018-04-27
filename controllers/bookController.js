8b77af5442348d0a6aceffac41b9b1748332c6e2
ceoehis <ceo.ehis@outlook.com>
2018-04-27 09:10:21 +0100

    add get method for book delete form

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -161,8 +161,23 @@ exports.book_create_post = [
 ];
 
 // Display book delete form on GET.
-exports.book_delete_get = function (req, res) {
-  res.send('NOT IMPLEMENTED: Book delete GET');
+exports.book_delete_get = function (req, res, next) {
+
+  async.parallel({
+    book: function(callback) {
+      Book.findById(req.params.id).exec(callback);
+    },
+    books_instances: function(callback) {
+      BookInstance.find({'book': req.params.id}).exec(callback);
+    },
+  }, function(err, results) {
+    if(err) { return next(err); }
+    if(results.book == null) {
+      res.redirect('/catalog/books');
+    }
+    console.log(results);
+    res.render('book_delete', { title: 'Delete Book', book: results.book, book_instances: results.books_instances});
+  });
 };
 
 // Handle book delete on POST.
