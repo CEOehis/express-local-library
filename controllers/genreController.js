a31098a780d2b3daa67069b2f93dd6b1bb0d6719
ceoehis <ceo.ehis@outlook.com>
2018-04-27 13:34:33 +0100

    handle genre update

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -160,6 +160,53 @@ exports.genre_update_get = function (req, res, next) {
 };
 
 // Handle Genre update on POST.
-exports.genre_update_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Genre update POST');
-};
+exports.genre_update_post = [
+
+  // Validate that the name field is not empty.
+  body('name', 'Genre name required').isLength({ min: 1 }).trim(),
+
+  body('name', 'Genre name is too short').isLength({ min: 4 }).trim(),
+
+  // Sanitize (trim and escape) the name field.
+  sanitizeBody('name').trim().escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data.
+    var genre = new Genre({
+      name: req.body.name,
+      _id: req.params.id
+    });
+
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors.array() });
+      return;
+    }
+    else {
+      // Data from form is valid.
+      // Check if Genre with same name already exists.
+      Genre.findOne({ 'name': req.body.name })
+        .exec(function (err, found_genre) {
+          if (err) { return next(err); }
+
+          if (found_genre) {
+            // Genre exists, redirect to its detail page.
+            res.redirect(found_genre.url);
+          }
+          else {
+            Genre.findByIdAndUpdate(req.params.id, genre, {}, function(err, thegenre) {
+              if (err) { return next(err); }
+
+              res.redirect(thegenre.url);
+            });
+          }
+        });
+    }
+  }
+];
